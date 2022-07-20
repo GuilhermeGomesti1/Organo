b4e97a4139c129f61eda326f932279067268d9df
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2022-07-20 19:35:29 -0300

    mudei name

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -37,7 +37,7 @@ const Formulario = (props) => {
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto 
                 obrigatorio={true} 
-                label="Nome"
+                label="Name"
                  placeholder="Digite seu nome" 
                  valor={nome}
                  aoAlterado={valor => setNome(valor)}
